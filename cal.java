aaadd0c9f790bd73abd1e03497cfc22872efac6f
santhoshnaveenkumar <santhosh.p.kite@gmail.com>
2025-02-28 06:49:34 +0000

    mul is committed

--- cal.java
+++ cal.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class cal {
-	    public static void main(String[] args) {
-		            int num1 = 5;
-			            int num2 = 10;
-				            int result = num1 * num2;
-					            System.out.println("Multiplication of " + num1 + " and " + num2 + " is: " + result);
-						        }
+    
+    // Function for multiplication
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input: First number
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        // Input: Second number
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        
+        // Perform multiplication
+        int multiplicationResult = multiply(num1, num2);
+        System.out.println("Multiplication result: " + multiplicationResult);
+        
+        // Close the scanner
+        scanner.close();
+    }
 }
