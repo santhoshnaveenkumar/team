2d0069a27e78458aeda76b322cbcfc8e2d580763
sushmitha <sushmithaprem9@gmail.com>
2025-02-28 06:06:56 +0000

    addition & subraction function

--- cal.java
+++ cal.java
@@ -1,1 +1,17 @@
+public class cal {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        // Example usage of the new methods
+        System.out.println("Addition of 5 and 3: " + add(5, 3));
+        System.out.println("Subtraction of 5 and 3: " + subtract(5, 3));
+    }
+}
 
