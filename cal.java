f6ca546a36a3783506b71dbe2aedae98e3b9890f
Vivasri .S <vivasrisengottuvel@gmail.com>
2025-02-28 12:02:21 +0530

    multiplication commit is added

--- cal.java
+++ cal.java
@@ -1,1 +1,8 @@
-
+public class cal {
+	    public static void main(String[] args) {
+		            int num1 = 5;
+			            int num2 = 10;
+				            int result = num1 * num2;
+					            System.out.println("Multiplication of " + num1 + " and " + num2 + " is: " + result);
+						        }
+}
